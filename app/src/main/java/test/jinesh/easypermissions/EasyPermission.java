fcf73138999c6b969b697bd9921ea637c40c2495
jineshfrancs <jineshfofficial@gmail.com>
2016-12-13 20:06:54 +0530

    Initial Commit

--- app/src/main/java/test/jinesh/easypermissions/EasyPermission.java
+++ app/src/main/java/test/jinesh/easypermissions/EasyPermission.java
@@ -13,41 +13,62 @@ import android.util.Log;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class EasyPermission  {
+/**
+ * Utility class for asking permission
+ *
+ * @author Jinesh Francis
+ */
+public class EasyPermission {
+    /**
+     * Request code for all permissions
+     */
     private static final int MY_PERMISSIONS_REQUEST = 100;
+    /**
+     * Activity reference for reflection
+     */
     private Activity runningActivity;
 
-    public void requestPermission(Activity runningActivity,String permission){
-        this.runningActivity=runningActivity;
+    /**
+     * method to request permission
+     *
+     * @param runningActivity current activity reference
+     * @param permission      permission to ask
+     */
+    public void requestPermission(Activity runningActivity, String permission) {
+        this.runningActivity = runningActivity;
 
         if (ContextCompat.checkSelfPermission(runningActivity,
                 permission)
                 != PackageManager.PERMISSION_GRANTED) {
 
             ActivityCompat.requestPermissions(runningActivity,
-                        new String[]{ permission},
-                        MY_PERMISSIONS_REQUEST);
+                    new String[]{permission},
+                    MY_PERMISSIONS_REQUEST);
 
-        }else {
-            callInterface(runningActivity,runningActivity.getClass(),permission,true);
+        } else {
+            callInterface(runningActivity, permission, true);
         }
     }
 
-
+    /**
+     * This method is called in onRequestPermissionsResult of the runningActivity
+     *
+     * @param requestCode  The request code of runningActivity.
+     * @param permissions  The requested permissions of runningActivity.
+     * @param grantResults The grant results for the corresponding permissions from runningActivity
+     */
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST: {
-
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    if(runningActivity!=null){
-                        callInterface(runningActivity,runningActivity.getClass(),permissions[0],true);
+                    if (runningActivity != null) {
+                        callInterface(runningActivity, permissions[0], true);
                     }
 
                 } else {
-                    if(runningActivity!=null){
-                        callInterface(runningActivity,runningActivity.getClass(),permissions[0],false);
+                    if (runningActivity != null) {
+                        callInterface(runningActivity, permissions[0], false);
                     }
 
                 }
@@ -56,16 +77,19 @@ public class EasyPermission  {
         }
     }
 
-    private void callInterface(Activity activity,Class<? extends Activity> aClass,String permission, boolean b) throws InterfaceNotImplementedException {
+    /**
+     * Method to call OnPermissionResult interface
+     */
+    private void callInterface(Activity activity, String permission, boolean b) throws InterfaceNotImplementedException {
         Method method = null;
         try {
-            method=aClass.getMethod("onPermissionResult",String.class,boolean.class);
+            method = activity.getClass().getMethod("onPermissionResult", String.class, boolean.class);
         } catch (NoSuchMethodException e) {
             throw new InterfaceNotImplementedException("please implement EasyPermission.OnPermissionResult interface in your activity to get the permissions result");
         }
-        if(method!=null) {
+        if (method != null) {
             try {
-                method.invoke(activity, permission,b);
+                method.invoke(activity, permission, b);
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
             } catch (InvocationTargetException e) {
@@ -74,10 +98,11 @@ public class EasyPermission  {
         }
     }
 
-    public interface OnPermissionResult{
-        void onPermissionResult(String permission,boolean isGranded);
+    public interface OnPermissionResult {
+        void onPermissionResult(String permission, boolean isGranded);
     }
-    public class InterfaceNotImplementedException extends RuntimeException{
+
+    public class InterfaceNotImplementedException extends RuntimeException {
         public InterfaceNotImplementedException(String message) {
             super(message);
         }
